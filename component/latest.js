93f125263a13efb17e2545a4ea93608bae95baf6
Matt Lunn <matt@mattlunn.me.uk>
2012-06-14 08:24:03 +0100

    Side by side title

--- component/latest.js
+++ component/latest.js
@@ -4,16 +4,18 @@
   var debug = true;
   var prefs = (function () {
     var key = 'mattlunn-settings'; // Avoid a key conflict
-    var latest = 3;
+    var latest = 5;
     
     try {
       prefs = JSON.parse(localStorage.getItem(key));
     } catch (e) {};
     
+    // Check the settings are restored correctly.
     if (typeof prefs !== "object" || prefs === null) {
       prefs = {};
     }
     
+    // Upgrade paths from previous versions.
     switch (prefs.version) {
       case undefined:
         prefs.ignoredList = [];
@@ -22,6 +24,9 @@
         };
       case 2:
         prefs.pref.promote = true;
+      case 3:
+      case 4:
+        prefs.pref.sideBySideTitle = true;
     }
     
     prefs.version = latest;
@@ -36,6 +41,9 @@
     debug && console.log(str);
   }
   
+  /** 
+   * Show the correct UI when a edit has been handled (accepted, rejected or ignored).
+   */
   function handleAction(edit, action) {
     var opt = prefs.pref.reaction;
     
@@ -46,12 +54,13 @@
     edit = edit.closest('.suggested-edit');
     
     if (opt === "prepend") {
-      var details = edit.find('a.question-hyperlink,a.answer-hyperlink');
+      // First as we add our own hyperlink for title UI improvement
+      var details = edit.find('a.question-hyperlink,a.answer-hyperlink').first();
       var href = details.prop('href');
       var title = details.clone().find('.diff-add').remove().end().text();
       var description = action + (action.slice(-1) === 'e' ? 'd' : 'ed');
 
-      edit.fadeOut('slow', function() {
+      edit.fadeOut('slow', function () {
           $('.suggested-edit:first').before([
             "<div class='answer-summary question-summary'><div class='summary'>",
             "<a href=\"" + href + "\" class=\"question-hyperlink\">" + title + "</a> ",
@@ -65,11 +74,15 @@
     }
   }
   
+  /**
+   * Settings form
+   */
   jQuery(document).ready(function ($) {
+    // Used to ensure we don't add any duplicate field names to the page.
     var prefix = 'se-edit-2-';
     
     $('#questions').parent().append([
-      '<form id="se-edit-2-settings" style="padding:5px; background: #eee; border: 1px solid #ccc;">',
+      '<form id="se-edit-2-settings" style="padding:5px; background: #eee; border: 1px solid #ccc; margin-top: 20px;">',
           '<p><strong>Suggested Edit Queue 2.0 Extension Settings (Note page may need to be refreshed for changes to take affect)</strong></p>',
           '<p>',
             '<label>',
@@ -86,9 +99,16 @@
                 '<input type="checkbox" name="se-edit-2-promote" class="pref">',
             '</label>',
           '</p>',
+          '<p>',
+            '<label>',
+                'Show title diff side by side:',
+                '<input type="checkbox" name="se-edit-2-sideBySideTitle" class="pref">',
+            '</label>',
+          '</p>',
       '</form>'
     ].join(''));
     
+    // When a pref is updated, update the preferences object.
     var theForm = $('#se-edit-2-settings').on('change', '.pref', function () {
       var val = (this.type === 'checkbox' ? this.checked : this.value);
       var name = this.name.slice(prefix.length);
@@ -98,12 +118,12 @@
       } else if (val === "0") {
         val = false;
       }
-      
-      console.log('Renaming ' + this.name + ' to ' + name + ' and setting to ' + val);
+
       prefs.pref[name] = val;
       prefs.save();
     });
     
+    // Show the correct options in the form on page load.
     Object.keys(prefs.pref).forEach(function (key) {
       var val = this[key];
       var el = theForm.find('.pref[name="' + prefix + key + '"]');
@@ -178,17 +198,19 @@
     $(document).ajaxSend(function (e, xhr, opts) {
       var matches = opts.url.match(/\/suggested-edit\/(\d+)\/vote\//);
       
+      // If it's a suggested edit AJAX request....
       if (matches !== null) {
         opts.dataFilter = function (data, type) {
           var id = matches[1];
-          
-          // This is a no-op if prefs.pref.reaction !== "fade", as the edit is removed anyway
+
           try {
             var obj = JSON.parse(data);
             var wasApproved = /( approval |>approved<)/.test(obj.Message || obj.MiniHtml);
             var wasRejected = /( reject |>rejected<)/.test(obj.Message || obj.MiniHtml);
                         
             xhr.done(function () {
+              // Very specific over approval/ reject as AJAX messages such as out-of-votes, or already-approved (and
+              // who knows what else) can be received.
               if (wasApproved || wasRejected) {
                 handleAction($('#suggested-edit-' + id), wasApproved ? 'approve' : 'reject');
               }
@@ -202,6 +224,7 @@
             }
           } catch (e) {};
           
+          // Return the data unchanged
           return data;
         }
       }
@@ -225,6 +248,46 @@
     });
     
     prefs.save();
+  });
+  
+  /**
+   * http://meta.stackoverflow.com/questions/112724/please-keep-title-edits-display-consistent-with-content-edits
+   */
+  jQuery(document).ready(function ($) {
+    if (prefs.pref.sideBySideTitle) {
+      $('a.question-hyperlink,a.answer-hyperlink > span').each(function () {
+        var self = $(this);
+        var other = $();
+        
+        var additions = self.children('.diff-add');
+        var deletions = self.children('.diff-delete');
+        
+        if (additions.length && deletions.length) {
+          other = self.clone();
+          other.find('.diff-delete').hide();
+          
+          // Only hide, as the "Consistent review effect" implementation uses the title when "move-to-top" is enabled.
+          // If we removed the additions, we'd get dodgy titles.
+          additions.hide();
+          
+          var diff = $([
+            '<div class="body-diffs">',
+              '<table class="full-html-diff">',
+                '<tbody>',
+                  '<tr>',
+                    '<td class="post-text"></td>',
+                    '<td class="gutter"></td>',
+                    '<td class="post-text"></td>',
+                  '</tr>',
+                '</tbody>',
+              '</table>',
+            '</div>'
+          ].join('')).insertBefore(self);
+          
+          diff.find('.post-text').first().append(self).end().last().append(other);
+        }
+      });
+    }
   });
 
 }());
